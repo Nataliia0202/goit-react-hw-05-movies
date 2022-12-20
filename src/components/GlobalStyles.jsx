db0ffd12f1659967c8ab2be7591f574807401c79
Nataliia Svystula <natashasvistula@gmail.com>
2022-12-20 20:16:59 +0200

    add

---
+++ src/components/GlobalStyles.jsx
@@ -0,0 +1,31 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        background-color: #9a9385;
+    }
+    #root {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+    code {
+        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+        monospace;
+    }
+    h1,h2,h3,h4,h5,h6,p {
+        margin: 0;
+    }
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+`;
